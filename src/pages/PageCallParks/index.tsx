3fa51eeabbb17e491a4879c5521247ec98a39c43
Akhil-eastern <a.thallapelly@easternenterprise.com>
2021-06-24 16:08:08 +0530

    park page ui

--- src/pages/PageCallParks/index.tsx
+++ src/pages/PageCallParks/index.tsx
@@ -1,6 +1,7 @@
 import UserItem from 'components/ContactUserItem'
 import CustomGradient from 'components/CustomGradient'
 import CustomHeader from 'components/CustomHeader'
+import CustomLayout from 'components/CustomLayout'
 import { RnText, RnTouchableOpacity } from 'components/Rn'
 import { observer } from 'mobx-react'
 import styles from 'pages/PageCallParks/Styles'
@@ -34,19 +35,9 @@ class PageCallParks extends React.Component<{
     return callParks2 ? park(selectedPark) : startCall(selectedPark || '')
   }
 
-  render() {
-    const authStore = getAuthStore() || {}
-    const { currentProfile } = authStore || {}
-    const { parks } = currentProfile || {}
-    const { selectedPark } = this.state
-
+  ParkComponent = (parks, selectedPark) => {
     return (
-      <CustomGradient>
-        <CustomHeader
-          onBack={Nav().backToPageCallManage}
-          description={'Your park numbers'}
-          title={'Park'}
-        />
+      <>
         {!parks.length && (
           <>
             <View style={styles.noParksContainer}>
@@ -71,7 +62,43 @@ class PageCallParks extends React.Component<{
             </RnTouchableOpacity>
           ))}
         </ScrollView>
-      </CustomGradient>
+      </>
+    )
+  }
+
+  render() {
+    const authStore = getAuthStore() || {}
+    const { currentProfile } = authStore || {}
+    const { parks } = currentProfile || {}
+    const { selectedPark } = this.state
+    const { callParks2 } = this.props
+
+    return (
+      <>
+        {!callParks2 && (
+          <>
+            <CustomLayout menu='keys' subMenu='parks'>
+              <View style={styles.parkContainer}>
+                <RnText style={styles.ParksText}>{'Park'}</RnText>
+                <RnText style={styles.noParksDesc}>
+                  {'Recent voicemails and calls'}
+                </RnText>
+              </View>
+              {this.ParkComponent(parks, selectedPark)}
+            </CustomLayout>
+          </>
+        )}
+        {!!callParks2 && (
+          <CustomGradient>
+            <CustomHeader
+              onBack={Nav().backToPageCallManage}
+              description={'Your park numbers'}
+              title={'Park'}
+            />
+            {this.ParkComponent(parks, selectedPark)}
+          </CustomGradient>
+        )}
+      </>
     )
   }
 }
