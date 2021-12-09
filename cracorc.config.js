28185b199109709dc079e15fedb43459ff14c97a
Akhil-eastern <a.thallapelly@easternenterprise.com>
2021-12-09 23:11:11 +0530

    ios build

---
+++ cracorc.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+
+const babel = require('./.babelrc')
+
+const nullAlias = path.join(__dirname, './src/polyfill/null.ts')
+
+module.exports = {
+  babel,
+  webpack: {
+    alias: {
+      'react-native': 'react-native-web',
+      'react-native-fast-image': 'react-native-web/dist/exports/Image',
+      'react-native-linear-gradient': 'react-native-web-linear-gradient',
+      'react-native-svg': 'react-native-svg-web',
+      'react-native-uuid': 'uuid',
+      'react-native-callkeep': nullAlias,
+      'react-native-fs': nullAlias,
+      'react-native-incall-manager': nullAlias,
+      'react-native-share': nullAlias,
+      'react-native-splash-screen': nullAlias,
+      'react-native-background-timer': nullAlias,
+      '@': path.resolve(__dirname, 'src/'),
+    },
+    configure: {
+      resolve: {
+        extensions: [
+          // Try to resolve `.web.*` first
+          '.web.js',
+          '.web.ts',
+          '.web.tsx',
+          '.js',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    },
+    plugins: [
+      new CircularDependencyPlugin({
+        exclude: /node_modules/,
+      }),
+    ],
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^@(.*)$': '<rootDir>/src$1',
+      },
+    },
+  },
+}
